2360dcbf2b68e1f767ee9425d022331af22fe5ac
Vladislav Zakharov <vl4disalex@gmail.com>
2025-09-01 00:42:47 +0300

    Complete all remaining sections and finalize work on the homepage.

---
+++ js/main.js
@@ -0,0 +1,10 @@
+const swiper = new Swiper(".swiper", {
+    effect: "fade",
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+      },
+      pagination: {
+        el: ".swiper-pagination",
+      },
+    });
