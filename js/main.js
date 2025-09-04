50775153ac44249bf9cbf2bf1a9505ae6a82fbd8
Vladislav Zakharov <vl4disalex@gmail.com>
2025-09-04 17:32:20 +0300

    Create catalog page with switching functionality, link all site pages, and complete the desktop version of the project.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,30 @@
-const swiper = new Swiper(".swiper", {
+
+  const tabsBtn = document.querySelectorAll('.tabs__btn-item');
+  const tabsContent = document.querySelectorAll('.tabs__content-item');
+
+  const openSelectedContent = (event) => {
+    const tabTarget = event.currentTarget
+    const button = tabTarget.dataset.button
+
+    tabsBtn.forEach(tabItem => {
+      tabItem.classList.remove('tabs__btn-item--active')
+    })
+
+    tabTarget.classList.add('tabs__btn-item--active')
+
+    tabsContent.forEach(contentItem => {
+      contentItem.classList.remove('tabs__content-item--active')
+    })
+
+    document.querySelector(`#${button}`).classList.add('tabs__content-item--active');
+  }
+
+  tabsBtn.forEach(tab => {
+    tab.addEventListener('click', openSelectedContent)
+  })
+
+
+  const swiper = new Swiper(".swiper", {
     effect: "fade",
     autoplay: {
         delay: 3000,
